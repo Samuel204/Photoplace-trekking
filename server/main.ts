0ca37c3066e374d36956ad73b130b371e5174272
NumenMirus <michaelbastauni@gmail.com>
2025-07-23 13:12:04 +0200

    Add detailed response for "/escursioni" endpoint

--- server/main.ts
+++ server/main.ts
@@ -4,9 +4,26 @@ const app = express()
 const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
+})
+
+app.get("/escursioni", (req: Request, res: Response) => {
+  res.send(
+    [{
+      id: 1,
+      name: "Escursione al Monte Bianco",
+      description: "Una splendida escursione al Monte Bianco.",
+      difficulty: "Alta"
+    },
+    {
+      id: 2,
+      name: "Escursione al Lago di Como",
+      description: "Un'escursione panoramica attorno al Lago di Como.",
+      difficulty: "Media"
+    }
+  ]);
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
